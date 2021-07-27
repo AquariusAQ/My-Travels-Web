3470455554aaf97100ca4bce08d813f093365a47
Aquarius <aquarius.aqaq@gmail.com>
2021-07-27 23:13:38 +0800

    add animation

--- TrainWeb/public/javascripts/toggle.js
+++ TrainWeb/public/javascripts/toggle.js
@@ -3,16 +3,25 @@ var part_name = ["main", "exhibitions", "tickets"];
 $(document).ready(function() {
     part_name.forEach(element => {
         $("#nav-"+element).click(function() {
-            hide_all();
-            $('#'+element).show();
-            $("#nav-"+element).addClass("active");
+            if ($("#nav-"+element).hasClass("active")) {
+                $('body,html').animate({scrollTop:0}, 200);
+            } else {
+                hide_all();
+                // $('#'+element).show();
+                setTimeout(() => {
+                    $('body,html').animate({scrollTop:0}, 100);
+                    $('#'+element).fadeIn(100);
+                    $("#nav-"+element).addClass("active");
+                }, 100);
+            }
         });
     });
 });
 
 var hide_all = () => {
     part_name.forEach(element => {
-        $("#"+element).hide();
+        // $("#"+element).hide();
+        $("#"+element).fadeOut(100);
         $("#nav-"+element).removeClass("active");
     });
 };
